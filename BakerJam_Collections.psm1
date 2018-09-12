dbe5e3e38c51d125db065bc41eb43053ee7a2ff8
jvbaker95 <jvbaker95@hotmail.com>
2018-09-12 08:27:24 -0400

    Update BakerJam_Collections.psm1

--- BakerJam_Collections.psm1
+++ BakerJam_Collections.psm1
@@ -1,4 +1,4 @@
 #Instead of importing discrete modules, get all modules in the folder that this is in.
-Foreach ($Module in $(Get-ChildItem $PSScriptRoot | Where-Object {($_.Name.contains("psm1")) -and ($_.Name -ne "BakerJam_Collections.psm1")})) {
+ForEach ($Module in $(Get-ChildItem $PSScriptRoot -Filter *psm1 | Where-Object {!$_.Name.Equals("BakerJam_Collections.psm1")})) {
     Import-Module $Module.FullName -DisableNameChecking
 }
