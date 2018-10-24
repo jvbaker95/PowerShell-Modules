8982423eadf7533e0d441933fda53b8221b83382
jvbaker95 <jvbaker95@hotmail.com>
2018-10-24 08:01:55 -0400

    Added comments

--- KeywordList.psm1
+++ KeywordList.psm1
@@ -1,3 +1,5 @@
+#Creates a object that accepts a string array; this allows the user to search strings via keywords via two methods: .getKeyword() and .containsKeyword().
+#REQUIRES -Version 5.0
 class KeywordList {
     [String[]]$Keywords
 
@@ -8,10 +10,11 @@ class KeywordList {
         return $null -ne ($this.Keywords | ? {$InputString -match $this._})
     }
     [Object]getKeyword([String]$InputString) {
-        return ($this.Keywords | ? { $InputString -match $_ })  # returns $true
+        return ($this.Keywords | ? { $InputString -match $_ })
     }
 }
 
+#Create a KeywordList object; allows the user to access object initialization via cmdlet.
 Function Create-KeywordList {
     param(
         [Parameter(Mandatory=$true)][String[]]$InputList
