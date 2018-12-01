78a7cfae27ed58e8dfa974e94f961aec40bef53d
jvbaker95 <jvbaker95@hotmail.com>
2018-12-01 15:51:25 -0500

    Update GUI-Functions.psm1

--- GUI-Functions.psm1
+++ GUI-Functions.psm1
@@ -16,16 +16,17 @@ Function Hide-PSWindow {
 #This will create a file prompt.
 Function Create-FilePrompt {
     Param(
-        [Parameter(Mandatory=$false)][Object]$InitialDirectory=$env:HOMEDRIVE
+        [Parameter(Mandatory=$false)][Object]$InitialDirectory=$env:HOMEDRIVE,
+        [Parameter(Mandatory=$false)][Object]$Title="Open"
     )
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
     $OpenFileDialog.initialDirectory = $InitialDirectory
     $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.Title = $Title
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filename
-
 } 
 
 #This will create form boxes where a target user can enter input in a UI-Box.
